c507eff133016bf52012be7017efa2b6e3e7b205
Aashmansaini <aashmansaini@gmail.com>
2026-05-12 16:51:37 +0530

    Refine university name with MSIT suffix

--- pages/index.js
+++ pages/index.js
@@ -276,7 +276,7 @@ export default function Home() {
                             {[
                                 {
                                     degree: "B.Tech in Information Technology",
-                                    school: "Guru Gobind Singh Indraprastha University",
+                                    school: "Guru Gobind Singh Indraprastha University(MSIT)",
                                     period: "2022 – 2026",
                                     stats: "GPA 7.0/10"
                                 },
