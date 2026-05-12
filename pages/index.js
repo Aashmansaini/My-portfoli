b695a4fadf6c3723a597c07589a800c86288485f
Aashmansaini <aashmansaini@gmail.com>
2026-05-12 16:48:16 +0530

    Update university name in education section

--- pages/index.js
+++ pages/index.js
@@ -276,7 +276,7 @@ export default function Home() {
                             {[
                                 {
                                     degree: "B.Tech in Information Technology",
-                                    school: "Maharaja Surajmal Institute of Technology, Delhi",
+                                    school: "Guru Gobind Singh Indraprastha University",
                                     period: "2022 – 2026",
                                     stats: "GPA 7.0/10"
                                 },
